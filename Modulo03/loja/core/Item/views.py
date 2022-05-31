86208df6e4f12746a46947b27b7b12a72f6fe19b
Edson <60894182+Edson-Padilha@users.noreply.github.com>
2022-05-31 13:01:55 -0300

    Delete Modulo03/loja directory
    Atualizar

--- Modulo03/loja/core/Item/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def lista_categorias(request):
-    pass
-
-def lista_itens(request):
-    pass
--- Modulo03/loja/core/Local/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def lista_estados(request):
-    return render(request,'lista_estados.html')
-
-def lista_cidades(request):
-    return render(request,'lista_cidades.html')
